b804eee90dccf77bd3814694a7c99298d41a5c6b
JinxPP <166177426+JinxPP@users.noreply.github.com>
2025-12-14 04:59:21 +0530

    Add files via upload

---
+++ curr-converter-script.js
@@ -0,0 +1,40 @@
+let select = document.querySelectorAll('.currency')
+let btn = document.getElementById('btn')
+let input = document.getElementById('input')
+fetch('https://api.frankfurter.dev/v1/currencies')
+.then(response => response.json())
+.then(response => displayDropDown(response))
+
+function displayDropDown(response){
+  let curr = Object.entries(response);
+  for(let i=0; i<curr.length; i++){
+    let opt = `<option value="${curr[i][0]}">${curr[i][0]}</option>`
+    select[0].innerHTML += opt
+    select[1].innerHTML += opt
+  }
+}
+
+btn.addEventListener('click',()=>{
+  let curr1 = select[0].value
+  let curr2 = select[1].value
+  let inputVal = input.value
+  if(curr1 === curr2)
+    alert('Please select different currency to convert')
+  else
+    convert(curr1, curr2, inputVal)
+});
+
+function convert(curr1, curr2, iputVal) {
+  const host = 'api.frankfurter.dev'
+fetch(`https://${host}/v1/latest?base=${curr1}&symbols=${curr2}`)
+  .then((resp) => resp.json())
+  .then((data) => {
+    //const convertedAmount = (inputVal * data.rates[to]).toFixed(2);
+    document.getElementById('result').value = Object.values(data.rates)[0]
+
+
+  });
+
+}
+
+
